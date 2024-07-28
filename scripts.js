1aeb5fec9311ecd94f91fff90a080dc62620b6f0
celiy <sasbrazilian@gmail.com>
2024-07-28 19:45:09 -0300

    quinto commit

--- scripts.js
+++ scripts.js
@@ -20,3 +20,62 @@ const elements = document.querySelectorAll('.caixaRounded');
 elements.forEach(el => {
     observer.observe(el, options);
 });
+
+//Iniciar animação das imagens de fundo1
+window.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.imagemfundo1');
+
+    const parallaxEffect = (scrollY) => {
+        images.forEach(image => {
+            image.style.transform = `translateY(${scrollY * 0.08}px)`;
+        });
+    };
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        parallaxEffect(scrollY);
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                window.addEventListener('scroll', handleScroll);
+            } else {
+                window.removeEventListener('scroll', handleScroll);
+            }
+        });
+    });
+
+    images.forEach(image => {
+        observer.observe(image);
+    });
+});
+//Iniciar animação das imagens de fundo2
+window.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.imagemfundo2');
+
+    const parallaxEffect = (scrollY) => {
+        images.forEach(image => {
+            image.style.transform = `translateY(${scrollY * 0.08}px)`;
+        });
+    };
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        parallaxEffect(scrollY);
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                window.addEventListener('scroll', handleScroll);
+            } else {
+                window.removeEventListener('scroll', handleScroll);
+            }
+        });
+    });
+
+    images.forEach(image => {
+        observer.observe(image);
+    });
+});
