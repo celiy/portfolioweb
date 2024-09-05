956cd399b1232146b294738f09b6e9cc7d74bd28
Aluno <aluno@ACADEMICO.CAMAQUA>
2024-09-04 21:54:21 -0300

    add image slideshow

--- scripts.js
+++ scripts.js
@@ -10,7 +10,6 @@ const observer1 = new IntersectionObserver((entries) => {
 const hiddenElements1 = document.querySelectorAll('.hidden');
 hiddenElements1.forEach((el) => observer1.observe(el));
 
-
 const observer2 = new IntersectionObserver((entries2) => {
     entries2.forEach((entry2) =>{
         if (entry2.isIntersecting){
@@ -23,5 +22,29 @@ const observer2 = new IntersectionObserver((entries2) => {
 const hiddenElements2 = document.querySelectorAll('.hidden2');
 hiddenElements2.forEach((el2) => observer2.observe(el2));
 
-var image = document.getElementById('imgslides');
-image.src = 'images/sobre_portfolio/projeto_1.png'
+let imgcounter = 1;
+const elemouse = document.getElementById('imgslides');
+let mousehover = false;
+function slideShow(){
+    //Detecta se o mouse está sem cima do slideshow
+    elemouse.addEventListener('mouseover', () => {
+        mousehover = true;
+    });
+    elemouse.addEventListener('mouseout', () => {
+        mousehover = false;
+    });
+    
+    //Se o mouse não estiver em cima, o slideshow continua novamente.
+    if (mousehover === false){
+        var image = document.getElementById('imgslides');
+        image.src = `images/sobre_portfolio/projeto_${imgcounter}.jpg`;
+        imgcounter++;
+        if (imgcounter > 5){
+            imgcounter = 1;
+        }
+        setTimeout(slideShow, 3000);
+    } else {
+        setTimeout(slideShow, 3000);
+    }
+}
+slideShow();
