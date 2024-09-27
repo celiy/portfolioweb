0874ab069a03e46f6b832b92509288ccda07b7fc
Diogo Carvalho Viegas <sasbrazilian@gmail.com>
2024-09-27 17:18:22 -0300

    add traslations for index page and navbar template

--- js/index.js
+++ js/index.js
@@ -1,20 +1,5 @@
 import {SlideAnimationObserver} from './classes/slide-in.js';
-import {eng_translations} from './translations/eng.js';
 
 const slideInFromLeft = new SlideAnimationObserver('hiddenL', 'showL');
 const slideInFromRight = new SlideAnimationObserver('hiddenR', 'showR');
 const slideInFromTop = new SlideAnimationObserver('hidden2', 'show2');
-
-//Esta parte faz a tradução do site para o idioma inglês
-const ids = [];
-for (const element of document.querySelectorAll('*[id]')) {
-    const id = element.id;
-    if (!isNaN(parseInt(id))) {
-        ids.push(id);
-    }
-}
-
-for (const id of ids) {
-    const element = document.getElementById(id);
-    element.innerHTML = eng_translations[id];
-}
---
+++ js/main.js
@@ -0,0 +1,31 @@
+import {eng_translations} from './translations/eng.js';
+
+const ulmenu = document.getElementById('ulmenu');
+
+ulmenu.innerHTML = `
+<li><a id="111" class="js-active" href="sobre.html">Sobre</a></li>
+<li><a id="222" class="js-active" href="projetos.html">Projetos</a></li>
+<li><a id="333" class="js-active" href="contato.html">Contato</a></li>
+<li><a id="444" class="js-active" href="index.html">Home</a></li>`;
+
+const active = document.querySelectorAll('.js-active');
+
+for (const item of active) {
+    if (item.href === window.location.href) {
+        item.classList.add('active');
+    }
+}
+
+//Esta parte faz a tradução do site para o idioma inglês
+const ids = [];
+for (const element of document.querySelectorAll('*[id]')) {
+    const id = element.id;
+    if (!isNaN(parseInt(id))) {
+        ids.push(id);
+    }
+}
+
+for (const id of ids) {
+    const element = document.getElementById(id);
+    element.innerHTML = eng_translations[id];
+}
