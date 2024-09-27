14b3746f3023904fcbb86bc8e0dbe13c7bd3c0cd
Diogo Carvalho Viegas <sasbrazilian@gmail.com>
2024-09-27 16:21:30 -0300

    add translation template

---
+++ js/translations/eng.js
@@ -0,0 +1,6 @@
+export const eng_translations = {
+    1:'Hello, my name is',
+    2:'Diogo Carvalho Viegas',
+    3:'I am a <span id="textoVermelho"> System Analyst and Develpoper </span>',
+    4:'I started my career as a programmer with the C programming language. Since then, I have been learning and expanding my knowledge. This is my portfolio, which I created not only to show my knowledge of HTML/CSS, but also my skills as a graphic designer. I am looking for new job opportunities and open to creating new projects. I am currently attending a higher education course in Systems Analysis and Development at IFSUL Campus Camaquã.'
+}
