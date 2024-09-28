e4be8de1cfa81eb2aaf0d20e16ce2c2254dbd32c
Diogo Carvalho Viegas <sasbrazilian@gmail.com>
2024-09-28 20:11:53 -0300

    add translation projects and async await for navbar

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 import {eng_translations} from './translations/eng.js';
 
-const ulmenu = document.getElementById('ulmenu');
+async function draw_navbar(){
+    const ulmenu = document.getElementById('ulmenu');
 
-ulmenu.innerHTML = `
-<li><a id="111" class="js-active" href="sobre.html">Sobre</a></li>
-<li><a id="222" class="js-active" href="projetos.html">Projetos</a></li>
-<li><a id="333" class="js-active" href="contato.html">Contato</a></li>
-<li><a id="444" class="js-active" href="index.html">Home</a></li>`;
+    ulmenu.innerHTML = `
+    <li><a id="111" class="js-active" href="sobre.html">Sobre</a></li>
+    <li><a id="222" class="js-active" href="projetos.html">Projetos</a></li>
+    <li><a id="333" class="js-active" href="contato.html">Contato</a></li>
+    <li><a id="444" class="js-active" href="index.html">Home</a></li>`;
 
-const active = document.querySelectorAll('.js-active');
+} draw_navbar().then(() => {
+    const active = document.querySelectorAll('.js-active');
 
-for (const item of active) {
-    if (item.href === window.location.href) {
-        item.classList.add('active');
+    for (const item of active) {
+        if (item.href === window.location.href) {
+            item.classList.add('active');
+        }
     }
-}
+});
 
 //Esta parte faz a tradução do site para o idioma inglês
 const ids = [];
